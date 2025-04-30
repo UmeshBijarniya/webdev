25e269964cd4a205723c88f426ff096805c2727f
UmeshBijarniya <u961000@gmail.com>
2025-04-30 19:17:25 +0000

    learning about datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code with newer syntax 
+
+// alert(3+3) // this works in browser not in node.js
+
+console.log(3+3)
+
+let name="umesh"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined => 
+//symbol => unoque
+
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
