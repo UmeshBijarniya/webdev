135d4529ded88b78c821faf2921b2d38c12ff9cc
UmeshBijarniya <u961000@gmail.com>
2025-04-30 20:04:41 +0000

    comparing

---
+++ 01_basics/comparision.js
@@ -0,0 +1,8 @@
+// console.log(2>1)
+// console.log(4>6)
+// console.log(1!=4)
+
+console.log(null>0)
+console.log(null==0)
+
+// === strictly check the given values eithour converting the type of dadtatypes
