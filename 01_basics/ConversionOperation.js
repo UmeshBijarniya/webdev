31b0e019bbf94e8fdb2fb4a1487302c5560ce115
UmeshBijarniya <u961000@gmail.com>
2025-04-30 19:47:58 +0000

    learning about conversion and operation od datatypes

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,41 @@
+let score = 20
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// if score = 32rnf then valueInNUmber = NaN
+// if score = NULL =>valueInNumber = 0
+// if score = true => valueInNUmber = 1
+
+// fro conversion in string use "string"  and for boolean use "boolean"
+
+// ***** operations ******
+
+let value=3
+let negValue= -value
+
+// console.log(negValue)
+
+// console.log(2**2)
+// console.log(2+2)
+// console.log(2/3)
+
+let str1="umesh"
+let str2="bijarniya"
+let str3=str1+str2
+console.log(str3)
+
+// string+string = string
+//string +number = string
+//number +string = string
+///number + number = number
+
+console.log(+true)  // gives 1 as output
+
+// number++ postfix
+// ++number prefix
+
