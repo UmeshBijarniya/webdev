4a2b8ceb790f236a1674c07d2eef6ff8237e1e92
UmeshBijarniya <u961000@gmail.com>
2025-05-03 17:33:42 +0000

    learning more about arrays

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -16,13 +16,30 @@ const myarr1 = new Array(7,6,5,4)
 
 // Slice and Splice
 
-console.log("A",myarr)
-const myn1 = myarr.slice(1,3)
-console.log("B",myn1)
+// console.log("A",myarr)
+// const myn1 = myarr.slice(1,3)
+// console.log("B",myn1)
 
-console.log(myarr)
+// console.log(myarr)
 
-const myn2 = myarr.splice(1,3)
-console.log("C",myn2)
-console.log(myarr)
+// const myn2 = myarr.splice(1,3)
+// console.log("C",myn2)
+// console.log(myarr)
+
+// // concarenating 2 arrays
+// const arr=[1,2,3,4]
+// const arr1=[5,6,7,8]
+// console.log(arr)
+// //make new array to use concat
+// const arr2=arr.concat(arr1)
+// console.log(arr2)
+
+// //another method is to use spread
+
+// const arr3=[...arr,...arr1]
+// console.log(arr3)
+
+console.log(Array.isArray("umesh"))
+console.log(Array.from("umesh"))
+console.log(Array.from({name:"umesh"}))  // this cannot convert this object in array so this gives empty array
 
