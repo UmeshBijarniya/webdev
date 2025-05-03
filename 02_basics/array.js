af7ac469e2177f785e4f982c5273060e3ebdd725
UmeshBijarniya <u961000@gmail.com>
2025-05-03 14:40:07 +0000

    array basics

---
+++ 02_basics/array.js
@@ -0,0 +1,28 @@
+const myarr = [2,3,6,4,325,4]
+// console.log(myarr)
+
+const myarr1 = new Array(7,6,5,4)
+// console.log(myarr1[3])
+
+// Array Methods 
+
+// myarr.push(44)
+// console.log(myarr)
+// myarr.pop()
+
+// console.log(myarr)
+// const newarr = myarr.join()
+// console.log(newarr)
+
+// Slice and Splice
+
+console.log("A",myarr)
+const myn1 = myarr.slice(1,3)
+console.log("B",myn1)
+
+console.log(myarr)
+
+const myn2 = myarr.splice(1,3)
+console.log("C",myn2)
+console.log(myarr)
+
